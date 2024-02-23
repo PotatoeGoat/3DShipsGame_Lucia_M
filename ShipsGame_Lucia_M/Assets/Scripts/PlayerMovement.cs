d7c128df68a5cec6e3dbb51664bd9100e5d1e012
PotatoeGoat <147033284+PotatoeGoat@users.noreply.github.com>
2024-02-23 12:21:15 +0000

    7

--- ShipsGame_Lucia_M/Assets/Scripts/PlayerMovement.cs
+++ ShipsGame_Lucia_M/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 5f;
-
+    float minPlayerSpeed = 5f;
 
     public float speed = 2f;
     float speedH;
     float speedV;
 
+    //acelerar
+
+    public float maxPlayerSpeed = 20f;
+    bool applyAceleration = false;
+
+    public float acelerationSpeed = 4f;
     
 
     private Rigidbody rb;
@@ -22,16 +28,20 @@ public class PlayerMovement : MonoBehaviour
         speedH = speed;
         speedV = speed;
 
-        Cursor.lockState = CursorLockMode.Locked;
+        applyAceleration = false;
+        
+
     }
 
     void Update()
     {
-        // Movimiento constante hacia adelante
-        rb.velocity = transform.forward * playerSpeed; 
-        
-
         MousePosition();
+
+        PlayerMovements();
+
+        Debug.Log(playerSpeed);
+
+
     }
 
     public void MousePosition()
@@ -40,7 +50,35 @@ public class PlayerMovement : MonoBehaviour
         float h = speedH * Input.GetAxis("Mouse X");
         float v = speedV * Input.GetAxis("Mouse Y");
 
-        transform.Rotate(-v, h, 0);
+        transform.Rotate(-v, h, 0f);
+    }
+
+    public void PlayerMovements()
+    {
+        // Movimiento constante hacia adelante
+        rb.velocity = transform.forward * playerSpeed;
+
+        if (Input.GetKeyDown("w"))
+        {
+            applyAceleration = true;
+            if (applyAceleration == true)
+            {
+                playerSpeed += (acelerationSpeed * Time.deltaTime);
+            }
+
+        }
+
+        if (Input.GetKeyUp("w"))
+        {
+            playerSpeed -= (acelerationSpeed * Time.deltaTime);
+            applyAceleration = false;
+
+            if (playerSpeed >= maxPlayerSpeed)
+            {
+                playerSpeed = minPlayerSpeed;
+            }
+        }
+
     }
 }
 
