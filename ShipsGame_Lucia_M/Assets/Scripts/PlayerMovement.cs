2f9b353f1c2881b598b5025288faa8e00f332daf
PotatoeGoat <147033284+PotatoeGoat@users.noreply.github.com>
2024-02-23 10:47:45 +0000

    5

--- ShipsGame_Lucia_M/Assets/Scripts/PlayerMovement.cs
+++ ShipsGame_Lucia_M/Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,28 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed = 5f;
 
 
-    public Camera cam;
-
     public float speed = 2f;
     float speedH;
     float speedV;
 
-    float axisX;
-    float axisY;
+    
 
     private Rigidbody rb;
 
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
 
         speedH = speed;
         speedV = speed;
-        cam = Camera.main;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         // Movimiento constante hacia adelante
-        //rb.AddForce(transform.forward * playerSpeed);
-
+        rb.velocity = transform.forward * playerSpeed; 
         
 
         MousePosition();
@@ -39,13 +36,11 @@ public class PlayerMovement : MonoBehaviour
 
     public void MousePosition()
     {
-        /*axisY += speedH * Input.GetAxis("Mouse X");
-        axisX += speedV * Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(-axisX, axisY, 90.0f);*/
+        //rotar hacia la posición del ratón
+        float h = speedH * Input.GetAxis("Mouse X");
+        float v = speedV * Input.GetAxis("Mouse Y");
 
-        /*Vector3 direction = Input.mousePosition - cam.WorldToScreenPoint(transform.position);
-        float angulo = Vector3.SignedAngle(Vector3.up, direction, Vector3.zero);
-        transform.rotation = (Quaternion.AngleAxis(angulo, Vector3.forward));*/
+        transform.Rotate(-v, h, 0);
     }
 }
 
